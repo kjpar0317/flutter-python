fe457b24630b5d1fe8bbc446137da2ccf9e7b18b
unknown <kjpar_kr@yahoo.co.kr>
2022-06-07 22:14:30 +0900

    자잘한 버그 수정

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -14,27 +14,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return GetMaterialApp(
-          title: "Application",
-          initialRoute: AppPages.INITIAL,
-          getPages: AppPages.routes,
-          builder: (context, widget) {
-            // ScreenUtil.init(context);
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_) => GetMaterialApp(
+              title: "Application",
+              initialRoute: AppPages.INITIAL,
+              getPages: AppPages.routes,
+              builder: (context, widget) {
+                // ScreenUtil.init(context);
 
-            return MediaQuery(
-              //Setting font does not change with system font size
-              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-              child: widget!,
-            );
-          },
-          theme: getTheme(context, Get.isDarkMode),
-          darkTheme: getTheme(context, Get.isDarkMode),
-        );
-      },
-    );
+                return MediaQuery(
+                  //Setting font does not change with system font size
+                  data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                  child: widget!,
+                );
+              },
+              theme: getTheme(context, Get.isDarkMode),
+              darkTheme: getTheme(context, Get.isDarkMode),
+            ));
   }
 }
